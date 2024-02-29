62aac41c750c53f9760f3e6e4cad488c6fa7d10f
inikki <nikita.lipatov@gmail.com>
2024-02-29 00:47:52 +0000

    add tests in UserResourceTest

--- web/src/test/java/org/games/xlspaceship/web/resources/UserResourceTest.java
+++ web/src/test/java/org/games/xlspaceship/web/resources/UserResourceTest.java
@@ -1,23 +1,30 @@
 package org.games.xlspaceship.web.resources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.games.xlspaceship.impl.game.GameStatus;
 import org.games.xlspaceship.impl.game.GameTurn;
 import org.games.xlspaceship.impl.game.GridStatus;
 import org.games.xlspaceship.impl.model.ErrorResponse;
+import org.games.xlspaceship.impl.model.SpaceshipProtocol;
 import org.games.xlspaceship.impl.services.GridServices;
 import org.games.xlspaceship.impl.services.RestServices;
 import org.games.xlspaceship.impl.services.ValidationServices;
 import org.games.xlspaceship.impl.services.XLSpaceshipServices;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.mockito.Mockito.*;
 
 public class UserResourceTest {
     
     @Test
-    public void testGetStatusByGameIdWhenGameExist() {
+    public void testGetStatusByGameIdWhenGameExist() throws Exception {
         var validServices = mock(ValidationServices.class);
         var restServices = mock(RestServices.class);
         var shipServices = mock(XLSpaceshipServices.class);
@@ -49,10 +56,29 @@ public class UserResourceTest {
         ResponseEntity<?> actualGameStatus = userResource.getStatusByGameId("game-1-2-3");
         Assertions.assertNotNull(actualGameStatus);
         Assertions.assertInstanceOf(GameStatus.class, actualGameStatus.getBody());
+        ObjectMapper objectMapper = new ObjectMapper();
+        Assertions.assertEquals("""
+                {
+                  "self" : {
+                    "user_id" : "nikilipa",
+                    "board" : [ ]
+                  },
+                  "opponent" : {
+                    "user_id" : "ai-500",
+                    "board" : [ ]
+                  },
+                  "game" : {
+                    "player_turn" : "nikilipa"
+                  }
+                }""",
+                objectMapper
+                        .writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(actualGameStatus.getBody())
+        );
     }
 
     @Test
-    public void testGetStatusByGameIdWhenGameDoesNotExist() {
+    public void testGetStatusByGameIdWhenGameDoesNotExist() throws Exception {
         var validServices = mock(ValidationServices.class);
         var restServices = mock(RestServices.class);
         var shipServices = mock(XLSpaceshipServices.class);
@@ -69,6 +95,105 @@ public class UserResourceTest {
         Assertions.assertInstanceOf(ErrorResponse.class, actualGameStatus.getBody());
         Assertions.assertEquals("Game ('game-1-2-3') is not found!",
                 ((ErrorResponse) actualGameStatus.getBody()).getError());
+        ObjectMapper objectMapper = new ObjectMapper();
+        Assertions.assertEquals("""
+                {
+                  "error_message" : "Game ('game-1-2-3') is not found!"
+                }""",
+                objectMapper
+                        .writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(actualGameStatus.getBody())
+        );
+    }
+
+    @Test
+    public void testCreateNewGameWithWrongPort() throws Exception {
+        var restServices = mock(RestServices.class);
+        var shipServices = mock(XLSpaceshipServices.class);
+        var gridServices = mock(GridServices.class);
+
+        var validServices = new ValidationServices(shipServices);
+
+        when(shipServices.isGameIdExist(anyString())).thenReturn(false);
+
+        UserResource userResource = new UserResource(
+                validServices, restServices, shipServices, gridServices
+        );
+
+        SpaceshipProtocol sp = new SpaceshipProtocol();
+        sp.setHostname("localhost");
+        sp.setPort("port");
+
+        ResponseEntity<?> errorResponse = userResource.createNewGame(sp);
+
+        Assertions.assertNotNull(errorResponse);
+        Assertions.assertInstanceOf(ErrorResponse.class, errorResponse.getBody());
+        ObjectMapper objectMapper = new ObjectMapper();
+        Assertions.assertEquals("""
+                {
+                  "error_message" : "Port should consist from numbers ('port')."
+                }""",
+                objectMapper
+                        .writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(errorResponse.getBody())
+        );
+    }
+
+    @Test
+    public void testCreateNewGameWithGatewayTimeout() throws Exception {
+        var restServices = mock(RestServices.class);
+        var shipServices = mock(XLSpaceshipServices.class);
+        var gridServices = mock(GridServices.class);
+
+        var validServices = new ValidationServices(shipServices);
+
+        GameTurn gameTurn = new GameTurn();
+        gameTurn.setPlayerTurn("nikilipa");
+
+        GridStatus myStatus = new GridStatus();
+        myStatus.setUserId("nikilipa");
+        GridStatus aiStatus = new GridStatus();
+        aiStatus.setUserId("ai-500");
+
+        GameStatus expectedGameStatus = new GameStatus();
+        expectedGameStatus.setHost("localhost");
+        expectedGameStatus.setPort(8080);
+        expectedGameStatus.setAliveShips(5);
+        expectedGameStatus.setGameTurn(gameTurn);
+        expectedGameStatus.setSelf(myStatus);
+        expectedGameStatus.setOpponent(aiStatus);
+
+        when(shipServices.status(anyString())).thenReturn(expectedGameStatus);
+        when(restServices.getCurrentHostname()).thenReturn("127.0.0.1");
+        when(restServices.getCurrentPort()).thenReturn(8081);
+        when(restServices.sendPostNewGameRequest(anyString(), anyInt(), any()))
+                .thenThrow(HttpServerErrorException.create(
+                        "Remote server timed out.",
+                        HttpStatus.GATEWAY_TIMEOUT,
+                        "Gateway timeout",
+                        new HttpHeaders(),
+                        "".getBytes(StandardCharsets.UTF_8),
+                        StandardCharsets.UTF_8
+                        )
+                );
+
+        UserResource userResource = new UserResource(
+                validServices, restServices, shipServices, gridServices
+        );
+
+        ResponseEntity<?> errorResponse = userResource.createNewGame("match-1-3-6");
+
+        Assertions.assertNotNull(errorResponse);
+        Assertions.assertInstanceOf(ErrorResponse.class, errorResponse.getBody());
+        ObjectMapper objectMapper = new ObjectMapper();
+        Assertions.assertEquals("""
+                {
+                  "error_message" : "Remote server timed out."
+                }""",
+                objectMapper
+                        .writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(errorResponse.getBody())
+        );
     }
 
 
