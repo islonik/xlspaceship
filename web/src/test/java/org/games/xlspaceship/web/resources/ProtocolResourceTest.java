589d7698b118fced8c53b2cb2e456fa62d7e8c2f
nikita.lipatov <nikita.lipatov@gmail.com>
2024-03-06 20:42:37 +0000

    refactoring

--- web/src/test/java/org/games/xlspaceship/web/resources/ProtocolResourceTest.java
+++ web/src/test/java/org/games/xlspaceship/web/resources/ProtocolResourceTest.java
@@ -23,30 +23,30 @@ public class ProtocolResourceTest {
         XLSpaceshipServices spaceshipServices = new XLSpaceshipServices(userServices, gridServices, gameServices, restServices, aiServices);
         ValidationServices validationServices = new ValidationServices(spaceshipServices);
 
-        when(userServices.getUserId()).thenReturn("ai");
-        when(userServices.getFullName()).thenReturn("Nikita");
+        when(userServices.getUserId()).thenReturn("AI");
+        when(userServices.getFullName()).thenReturn("AI-1176");
         when(gameServices.nextUniqueGameId()).thenReturn("match-1-2");
         when(gridServices.newUnknownGrid()).thenReturn(new Grid());
         when(gridServices.newRandomGrid()).thenReturn(new Grid());
-        when(gameServices.chooseStartingPlayer(anyString(), anyString())).thenReturn("ai");
+        when(gameServices.chooseStartingPlayer(anyString(), anyString())).thenReturn("AI");
 
         ProtocolResource protocolResource = new ProtocolResource(validationServices, spaceshipServices);
         NewGameResponse newGameResponse = protocolResource.createNewGame(createNewGameRequest());
 
         Assertions.assertNotNull(newGameResponse);
-        Assertions.assertEquals("ai", newGameResponse.getUserId());
-        Assertions.assertEquals("Nikita", newGameResponse.getFullName());
+        Assertions.assertEquals("AI", newGameResponse.getUserId());
+        Assertions.assertEquals("AI-1176", newGameResponse.getFullName());
         Assertions.assertEquals("match-1-2", newGameResponse.getGameId());
-        Assertions.assertEquals("ai", newGameResponse.getStarting());
+        Assertions.assertEquals("AI", newGameResponse.getStarting());
     }
 
     private NewGameRequest createNewGameRequest() {
         NewGameRequest newGameRequest = new NewGameRequest();
-        newGameRequest.setFullName("Nikita");
-        newGameRequest.setUserId("nl");
+        newGameRequest.setFullName("Nikita"); // full name of the user who initiated the request
+        newGameRequest.setUserId("nikilipa"); // id of the user who initiated the request
         SpaceshipProtocol sp = new SpaceshipProtocol();
-        sp.setHostname("localhost");
-        sp.setPort("8080");
+        sp.setHostname("localhost"); // server which initiated the request
+        sp.setPort("8077"); // port of the server which initiated the request
         newGameRequest.setSpaceshipProtocol(sp);
         return newGameRequest;
     }
